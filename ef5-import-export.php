92d2ecf9e15e213250038b227794beb3c94190b5
vannha <phongnhatk21@gmail.com>
2019-09-04 13:51:24 +0700

    commit -

--- ef5-import-export.php
+++ ef5-import-export.php
@@ -80,7 +80,7 @@ if (!class_exists('EF5_Import_Export')) {
             $current_theme = wp_get_theme();
             $this->theme_name = $current_theme->get('Name');
             $this->theme_text_domain = $current_theme->get('TextDomain');
-            add_submenu_page('tools.php', esc_html__('Import Demo1', EF5_IE_TEXT_DOMAIN), esc_html__('Import Demo2', EF5_IE_TEXT_DOMAIN), 'manage_options', 'ef5-import', array($this, 'ef5_import_demo_page'));
+            add_submenu_page('tools.php', esc_html__('Import Demo', EF5_IE_TEXT_DOMAIN), esc_html__('Import Demo', EF5_IE_TEXT_DOMAIN), 'manage_options', 'ef5-import', array($this, 'ef5_import_demo_page'));
         }
 
         public function ef5_import_demo_page()
